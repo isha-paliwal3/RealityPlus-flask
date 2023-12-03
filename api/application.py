fa10cf8455ae398b3b8c7744188fe8afa2c1df69
isha-paliwal3 <93034879+isha-paliwal3@users.noreply.github.com>
2023-12-03 17:18:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/application.py
@@ -0,0 +1,101 @@
+import os
+from time import sleep
+from packaging import version
+from flask import Flask, request, jsonify
+import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Check OpenAI version is correct
+required_version = version.parse("1.1.1")
+current_version = version.parse(openai.__version__)
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+if current_version < required_version:
+  raise ValueError(f"Error: OpenAI version {openai.__version__}"
+                   " is less than the required version 1.1.1")
+else:
+  print("OpenAI version is compatible.")
+
+# Start Flask app
+app = Flask(__name__)
+
+# Init client
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def createAssistant(client, instructions):
+
+  assistant = client.beta.assistants.create(
+    instructions=instructions,
+    model="gpt-4-1106-preview",
+    )
+  
+  assistant_id = assistant.id
+  
+  return assistant_id
+
+@app.route('/createAssistant', methods=['POST'])
+def create_assistant():
+  data = request.json
+  instructions = data.get('instructions', '')
+
+  if not instructions:
+    return jsonify({"error": "Missing instructions"}), 400
+
+  assistant_id = createAssistant(client, instructions)
+  return jsonify({"assistant_id": assistant_id})
+
+
+@app.route('/start', methods=['POST'])
+def start_conversation():
+  data = request.json
+  assistant_id = data.get('assistant_id')
+
+  if not assistant_id:
+    return jsonify({"error": "Missing assistant_id"}), 400
+
+  thread = client.beta.threads.create()
+  return jsonify({"thread_id": thread.id})
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+  data = request.json
+  thread_id = data.get('thread_id')
+  assistant_id = data.get('assistant_id')
+  user_input = data.get('message', '')
+
+  if not thread_id:
+    print("Error: Missing thread_id") 
+    return jsonify({"error": "Missing thread_id"}), 400
+
+  print(f"Received message: {user_input} for thread ID: {thread_id}")
+
+
+  client.beta.threads.messages.create(thread_id=thread_id,
+                                      role="user",
+                                      content=user_input)
+
+
+  run = client.beta.threads.runs.create(thread_id=thread_id,
+                                        assistant_id=assistant_id)
+
+  while True:
+    run_status = client.beta.threads.runs.retrieve(thread_id=thread_id,
+                                                   run_id=run.id)
+    print(f"Run status: {run_status.status}")
+    if run_status.status == 'completed':
+      break
+    sleep(1)  
+
+  messages = client.beta.threads.messages.list(thread_id=thread_id)
+  response = messages.data[0].content[0].text.value
+
+  print(f"Assistant response: {response}")
+  return jsonify({"response": response})
+
+# Run server
+if __name__ == '__main__':
+  app.run(debug=True)
